4b28e6183af2db7118d78e80d8999ae2a39b7240
jhihguan <imdiedie@gmail.com>
2014-09-02 01:35:57 -0700

    add require libs

--- web-app-boilerplate/js/main.js
+++ web-app-boilerplate/js/main.js
@@ -1,1 +1,38 @@
+require.config({
+  // paths....
+  paths: {
+    'jquery': 'libs/jquery-1.11.0.min',
+    'underscore': 'libs/underscore-1.7.0',
+    'backbone': 'libs/backbone-1.1.2',
+    'bootstrap': 'libs/bootstrap'
+  },
+  // make js to module(jquery have already do that)
+  shim: {
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      // backbone has a dependency based on underscore, jquery
+      deps: [
+        'underscore',
+        'jquery'
+      ],
+      exports: 'Backbone'
+    },
+    // bootstrap does not export, it add some function to jquery
+    'bootstrap': {
+      deps: [
+        'jquery'
+      ]
+    }
+  }
 
+});
+
+require([
+  'backbone'
+  ], function (Backbone) {
+    'use strict';
+
+    console.log(Backbone);
+  });
